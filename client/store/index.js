7c459dd2a0874613846ea363298a9511a26a347a
Chidi Benedict <c4benni@gmail.com>
2021-12-08 04:22:51 +0100

    Removed client folder

--- client/store/index.js
+++
@@ -1,9 +0,0 @@
-import Mutations from '~/utils/vuex/mutations'
-import State from '~/utils/vuex/state'
-import Getters from '~/utils/vuex/getters'
-
-export const state = State
-
-export const mutations = Mutations
-
-export const getters = Getters
