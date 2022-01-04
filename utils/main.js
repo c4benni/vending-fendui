667a7019d41c22d53ccd8c79c9f969b9148e76a9
Chidi Benedict <c4benni@gmail.com>
2022-01-04 19:10:26 +0100

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -659,3 +659,6 @@ export const serviceCall = async (axiosCall) => {
     }
   }
 }
+
+export const isProduction = process.env.NODE_ENV === 'production'
+export const isTesting = process.env.NODE_ENV === 'test'
